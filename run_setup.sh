59e9fcf60a9e6ad7027d1891e10f2e82a7d9d6b6
Sean Ruckle <seanruckle@gmail.com>
2017-04-15 13:25:07 -0700

    remove s

--- run_setup.sh
+++ run_setup.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 mkdir /home/$1/code
-curl -o /home/$1/setup.sh https://gitub.com/seanruckle/BajaSae-2017-Production/raw/master/setup.sh
+curl -o /home/$1/setup.sh http://gitub.com/seanruckle/BajaSae-2017-Production/raw/master/setup.sh
 chmod a+rwx /home/$1/setup.sh
 /home/$1/setup.sh $1
 
