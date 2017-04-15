81d6c67633c8bf761b0d5afd8465629a96efa920
Sean Ruckle <seanruckle@gmail.com>
2017-04-15 13:53:59 -0700

    Removed duplicate mkdir ~Kyle

--- run_setup.sh
+++ run_setup.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-mkdir /home/$1/code
 curl -o /home/$1/setup.sh https://raw.githubusercontent.com/seanruckle/BajaSae-2017-Production/master/setup.sh
 chmod a+rwx /home/$1/setup.sh
 /home/$1/setup.sh $1
